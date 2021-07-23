84ed0b79e65167d761facef3ad0ecef63786ab4b
Erik Lundin <c.erik.lundin@gmail.com>
2021-07-23 23:48:07 +0200

    minor POST request to add tickers to db working

--- server/server.py
+++ server/server.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, request
+from data_management import DataManager
 
 app = Flask(__name__)
+data_manager = DataManager()
 
 @app.route('/')
 def base():
     return 'hello world!'
 
-@app.route('/dashboard')
-def dashboard():
+@app.route('/post/ticker/<string:ticker>,<string:name>')
+def new_ticker(ticker, name):
     
+    data_manager.add_ticker(ticker, name)
+    return(f'Ticker requested: {ticker, name}')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000)
+    app.run(host='127.0.0.1', port=5000, debug=True)
--- server/testing.py
+++ server/testing.py
@@ -1,5 +1,1 @@
-from data_management import DataManager
-
-dm = DataManager()
-
-dm.add_missing_daily_data()
+import requests
