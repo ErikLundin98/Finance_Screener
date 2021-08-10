cef9955279b9fdca7a823bdd69159c89321d9448
Erik Lundin <c.erik.lundin@gmail.com>
2021-08-11 00:28:33 +0200

    sort of working portfolio query function

--- database/create_procedures.sql
+++ database/create_procedures.sql
@@ -1,25 +1,40 @@
-CREATE OR REPLACE PROCEDURE portfolio_returns(user_name VARCHAR(30))
-RETURNS TABLE AS
+CREATE OR REPLACE FUNCTION portfolio_prices_JSON(user_name VARCHAR(30))
+RETURNS TABLE(date DATE, ticker_info jsonb) AS
 $$
-SELECT 
+
+SELECT d.date AS date, 
+       JSONB_OBJECT_AGG(d.ticker, d.adjusted_close) as ticker_info
+FROM daily d
+WHERE d.ticker IN (SELECT ticker FROM portfolios WHERE portfolios.user_name=user_name)
+GROUP BY d.date
+ORDER BY date ASC
+
 $$
 LANGUAGE SQL;
 
-WITH ctx AS (
-    SELECT date,
-            ticker,
-            adjusted_close
-            
+SELECT * FROM portfolio_prices_JSON('snigeln_mjau')
+/*
+DECLARE @cols AS NVARCHAR(MAX), @query  AS NVARCHAR(MAX);
 
-)
+select @cols = STUFF((SELECT distinct ',' + QUOTENAME(c.ticker) 
+FROM daily c
+    FOR XML PATH(''), TYPE
+    ).value('.', 'NVARCHAR(MAX)') 
+,1,1,'')
 
-with context as(select ticker,
-                    name, 
-                    row_number() over(partition by id order by name) as rn
-             from table_name)
-select id,
-        [1] as name1,
-        [2] as name2,
-        [3] as name3
- from cte
- pivot(max(name) for rn in([1],[2],[3]))p
+
+set @query = 'SELECT [date], ' + @cols + ' from 
+        (
+           SELECT  [date]
+           ,[ticker]
+           ,[adjusted_close]
+           FROM daily
+        ) x
+        pivot 
+        (
+            min([adjusted_close])
+            for [ticker] in (' + @cols + ')
+        ) p '
+
+execute(@query)
+*/
