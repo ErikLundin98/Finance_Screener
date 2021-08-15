d543ac383dd0e599fda8cd83a430d3a618289d8b
Erik Lundin <c.erik.lundin@gmail.com>
2021-08-16 00:42:56 +0200

    add range-returns to asset_indicators materialized view

--- database/technical_views.sql
+++ database/technical_views.sql
@@ -28,16 +28,26 @@ CREATE OR REPLACE AGGREGATE geomean(value float8) (
 DROP MATERIALIZED VIEW IF EXISTS asset_indicators;
 CREATE MATERIALIZED VIEW asset_indicators
 AS 
-WITH one_year AS (
+WITH dates AS (
+    SELECT
+        CURRENT_DATE AS today,
+        CAST(CURRENT_DATE - interval '1 year' AS DATE) AS ONE_Y_first_date,
+        (SELECT MAX(first_date) FROM used_dates AS AT_first_date)
+),
+ticker_returns AS (
+    SELECT
+        ticker,
+        range_return(ticker, dates.ONE_Y_first_date, dates.today, 'arit') AS ONE_Y_arit_return
+    FROM used_tickers, dates
+),
+one_year AS (
     SELECT
         ticker,
         stddev(arithmetic_return) AS ONE_Y_arit_stdev,
         geomean(arithmetic_return) AS ONE_Y_arit_geomean,
         stddev(logarithmic_return) AS ONE_Y_log_stdev,
-        avg(logarithmic_return) AS ONE_Y_log_aritmean,
-        CURRENT_DATE - interval '1 year' AS ONE_Y_first_date,
-        (SELECT range_arit_return(ticker, CAST(CURRENT_DATE - interval '1 year' AS DATE), CURRENT_DATE)) AS ONE_Y_arit_return
-    FROM daily_returns WHERE date >= ONE_Y_first_date
+        avg(logarithmic_return) AS ONE_Y_log_aritmean
+    FROM daily_returns WHERE date >= CURRENT_DATE - interval '1 year'
     GROUP BY ticker
 ),
 all_time AS (
@@ -48,13 +58,17 @@ all_time AS (
         stddev(logarithmic_return) AS AT_log_stdev,
         avg(logarithmic_return) AS AT_log_aritmean,
         (SELECT MAX(first_date) FROM used_dates AS AT_first_date)
-    FROM daily_returns WHERE date >= AT_first_date
+    FROM daily_returns WHERE date >= (SELECT MAX(first_date) FROM used_dates)
     GROUP BY ticker
 )
 SELECT 
     used_tickers.ticker,
+    ticker_returns.ONE_Y_arit_return,
     ONE_Y_arit_stdev, ONE_Y_arit_geomean, ONE_Y_log_stdev, ONE_Y_log_aritmean,
     AT_arit_stdev, AT_arit_geomean, AT_log_stdev, AT_log_aritmean
     FROM used_tickers
     JOIN one_year ON used_tickers.ticker = one_year.ticker
-    JOIN all_time ON one_year.ticker = all_time.ticker;
+    JOIN all_time ON one_year.ticker = all_time.ticker
+    JOIN ticker_returns ON ticker_returns.ticker = used_tickers.ticker;
+
+SELECT * FROM asset_indicators;
