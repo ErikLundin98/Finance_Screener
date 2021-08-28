7616cf7a0e5120e28e1836a3b8a3764238e7de3e
Erik Lundin <c.erik.lundin@gmail.com>
2021-08-28 20:24:45 +0100

    remove constants table definition

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -5,12 +5,6 @@ DROP TABLE IF EXISTS exchanges CASCADE;
 DROP TABLE IF EXISTS investors CASCADE;
 DROP TABLE IF EXISTS portfolios CASCADE;
 
-/* Table defining constants */
-
-CREATE TABLE constants(
-    
-)
-
 /*Table that contains the list of tickers that are to be used in the database*/
 
 CREATE TABLE used_tickers(
